5950f89a2730657151921a171e22e79575f1a044
Floriian <lovasifloriian@outlook.hu>
2023-06-16 13:07:30 +0200

    fix seeding script

--- src/seeds/Cat.seed.ts
+++ src/seeds/Cat.seed.ts
@@ -2,15 +2,21 @@ import { dataSource } from "../config/Datasource";
 import { CatEntity } from "../entities/Cat.entity";
 import { faker } from "@faker-js/faker";
 
-async function main() {
-  const catRepository = dataSource.getRepository(CatEntity);
-  for (let i = 0; i <= 10; i++) {
-    const cat = new CatEntity();
-    cat.catName = faker.animal.cat();
-    cat.specie = faker.lorem.sentence({ min: 1, max: 1 });
-    await catRepository.save(cat);
-    console.log(`SEED: Cat created with ${cat.id}.`);
-  }
+function main() {
+  dataSource
+    .initialize()
+    .then(async (db) => {
+      const catRepository = dataSource.getRepository(CatEntity);
+      for (let i = 0; i <= 10; i++) {
+        const cat = new CatEntity();
+        cat.catName = faker.animal.cat();
+        cat.specie = faker.lorem.sentence({ min: 1, max: 1 });
+        cat.age = faker.number.int({ min: 1, max: 10 });
+        await catRepository.save(cat);
+        console.log(`SEED: Cat created with ${cat.id}.`);
+      }
+    })
+    .then(() => process.exit(0));
 }
 
 main();
