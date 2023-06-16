642190adb642fd2e013dbf50d6e3f62cbed24629
Floriian <lovasifloriian@outlook.hu>
2023-06-16 14:07:37 +0200

    fix cat seed

--- src/seeds/Cat.seed.ts
+++ src/seeds/Cat.seed.ts
@@ -12,6 +12,7 @@ function main() {
         cat.catName = faker.animal.cat();
         cat.specie = faker.lorem.sentence({ min: 1, max: 1 });
         cat.age = faker.number.int({ min: 1, max: 10 });
+        cat.sex = faker.person.sex() ? "FEMALE" : "MALE";
         await catRepository.save(cat);
         console.log(`SEED: Cat created with ${cat.id}.`);
       }
